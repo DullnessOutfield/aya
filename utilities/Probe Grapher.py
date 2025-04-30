901106d294cb969a86b790a1c920bc3995fb4e47
DullnessOutfield <DullnessOutfield@protonmail.com>
2025-04-30 12:41:40 -0400

    Probe Grapher works now

--- utilities/Probe Grapher.py
+++ utilities/Probe Grapher.py
@@ -1,9 +1,6 @@
-import kismetdb, json, os
 from pathlib import Path
-from glob import glob
 import networkx as nx
 import argparse
-import pandas as pd
 import aya
 from aya import KismetDevice
 
@@ -11,11 +8,10 @@ DESIRED_TYPES = ['Wi-Fi Device', 'Wi-Fi Client', 'Wi-Fi Bridged']
 BASEPATH = './Data/'
 
 parser = argparse.ArgumentParser()
-parser.add_argument("surveys", nargs='+', default="folder1")
+parser.add_argument("projects", nargs='+', default="folder1")
 args = parser.parse_args()
 
-All_SSIDs = []
-dictionary = {}
+
 
 def ProcessProject(project: Path):
     project_dictionary = {}
@@ -23,55 +19,59 @@ def ProcessProject(project: Path):
         devices = aya.getSTAs(kismet_file)
         for device in devices:
             if device.mac not in project_dictionary:
-                node = GetNodeData(device)
-                project_dictionary[device.mac] = node
+                project_dictionary[device.mac] = device.probedSSIDs
             else:
-                project_dictionary[device.mac]['SSID'] += device.probedSSIDs
+                project_dictionary[device.mac] += device.probedSSIDs
     project_dictionary = {i:project_dictionary[i]\
                         for i in project_dictionary\
-                        if len(project_dictionary[i]['SSID']) > 0}
-    project_hashdict = {hash(frozenset(project_dictionary[i]['SSID']+[project_name])):
-                        {'MSN': project_name,
-                         'MAC': project_dictionary[i]['MAC'],
-                         'NAME': project_dictionary[i]['NAME'],
-                        'SSID': project_dictionary[i]['SSID'],
-                         'TYPE': 'DEVICE'}
-                        for i in project_dictionary}
-    return project_hashdict
+                        if len(project_dictionary[i]) > 0}
+    return project_dictionary
 
 def GetNodeData(dev: KismetDevice):
-    devnode = {'ID': len(dictionary),
-                       'NAME': dev.name,
+    devnode = {        'NAME': dev.name,
                        'MAC': dev.mac,
                        'SSID': dev.probedSSIDs}
     return devnode
-                    
-def GenerateGraph(device_dictionary, SSID_dictionary):    
-    nodes = [device_dictionary[i] for i in device_dictionary]
-    SSID_list = [{'NAME': i, 'TYPE':'SSID', 'TimesSeen': 0, 'SeenAt': []} for i in SSID_dictionary]
-    nodes += SSID_list
-    G = nx.DiGraph()
-    G.add_nodes_from(enumerate(nodes))
-    for idx,node in enumerate(nodes):
-        if 'SSID' in node.keys():
-            for ssid in node['SSID']:
-                SSID_ID = (SSID_dictionary.index(ssid))+len(device_dictionary)
-                Loc_List = G.nodes()[SSID_ID]['SeenAt']
-                if not(node['MSN'] in Loc_List):
-                    G.nodes()[SSID_ID]['SeenAt'].append(node['MSN'])
-                    G.nodes()[SSID_ID]['TimesSeen'] += 1
-                G.add_edge(idx, SSID_ID)
-    print(len(G.nodes()))
-    nx.write_gml(G,'test.gml')
+
+def isProbeSubset(mac1, mac2):
+    return set(mac1).issubset(set(mac2))
+
+def PruneDict(file_dictionary):
+    """Remove keys whose value list is a subset of another's in the same dict."""
+    keys = list(file_dictionary.keys())
+    to_remove = set()
+    for i in range(len(keys)):
+        for j in range(len(keys)):
+            set(file_dictionary[keys[i]])
+            if i != j and isProbeSubset(file_dictionary[keys[i]], file_dictionary[keys[j]]):
+                to_remove.add(keys[i])
+    return {k: v for k, v in file_dictionary.items() if k not in to_remove}
+
+
+def GenerateGraph(overall_dict):
+    output = Path(aya.getBasePath() / 'graph.gml')
+    Graph = nx.Graph()
+    ssid_nodes = set()
+    for project_name, project_dict in overall_dict.items():
+        project_dict = PruneDict(project_dict)
+        for mac, node in project_dict.items():
+            Graph.add_node(mac, label=mac, type="mac", project=project_name)
+
+            for ssid in node:
+                if ssid not in ssid_nodes:
+                    Graph.add_node(ssid, label=ssid, type='ssid')
+                    ssid_nodes.add(ssid)
+                Graph.add_edge(mac, ssid)
+    nx.write_gml(Graph, output)
 
 def main():
     basepath = aya.getBasePath()
     nodes = {}
-    projects: list[Path] = [basepath / project for project in args.project]
+    projects: list[Path] = [basepath / project for project in args.projects]
     aya.CheckFilepaths(projects)
     for project in projects:
-        nodes = ProcessProject(project)
-    GenerateGraph(dictionary, All_SSIDs)
+        nodes[project.name] = ProcessProject(project)
+    GenerateGraph(nodes)
     
 
 if __name__ == '__main__':
