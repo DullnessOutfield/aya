7063de5bbd7f81025be623506ed1ac19929d710f
DullnessOutfield <DullnessOutfield@protonmail.com>
2025-05-13 21:54:36 -0400

    Added WigleDevice class

--- aya/classes.py
+++ aya/classes.py
@@ -26,6 +26,7 @@ class WirelessDevice(Device):
     device_type: Optional[str] = None
 
 
+
 @dataclass
 class WiFiDevice(WirelessDevice):
     """WiFi device with a MAC address as its identifier."""
@@ -34,6 +35,44 @@ class WiFiDevice(WirelessDevice):
     def mac(self):
         return self.identifier
 
+@dataclass
+class WigleDevice(WiFiDevice):
+    '''
+    [BSSID],[SSID],[Capabilities],[First timestamp seen],[Channel],[Frequency],[RSSI],[Latitude],[Longitude],[Altitude],[Accuracy],[RCOIs],[MfgrId],[Type]
+    1a:9f:ee:5c:71:c6,Scampoodle,[WPA2-EAP-CCMP][ESS],2018-08-01 13:08:27,161,5805,-43,37.76578028,-123.45919439,67,3.2160000801086426,5A03BA0000 BAA2D00000 BAA2D02000,,WIFI
+    '''
+    capabilities: str
+    first_seen: datetime
+    channel: int
+    frequency: int
+    rssi: int
+    lat: float
+    lon: float
+    alt: float
+    accuracy: float
+    # RCOI: idk,
+    # MfgrId: idk
+    def from_record(cls, row):
+        '''
+        Create a WigleDevice from a row in a wiglecsv file
+        '''
+        if isinstance(row, str):
+            row = row.split(',')
+        
+        return WigleDevice(
+            identifier = row[0],
+            name = row[1],
+            capabilities = row[2],
+            first_seen = datetime.fromtimestamp(row[3]),
+            channel = row[4],
+            frequency = row[5],
+            rssi = row[6],
+            lat = row[7],
+            lon = row[8],
+            alt = row[9],
+            accuracy = row[10],
+            device_type = 'Wi-Fi AP'
+        )
 
 # Simple factory function to create devices from MAC addresses
 def create_wifi_device(mac_address: str, **kwargs) -> WiFiDevice:
