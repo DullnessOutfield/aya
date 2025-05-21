6238c1be7a71bdd23327fbafb437048076e55846
DullnessOutfield <DullnessOutfield@protonmail.com>
2025-05-21 16:53:41 -0400

    Add: Very basic pcap->WiFiDevice creation

--- aya/pcap/lib.py
+++ aya/pcap/lib.py
@@ -1,8 +1,52 @@
+from __future__ import annotations
+
+from pathlib import Path
+
 import pyshark
+from pyshark.packet.packet import Packet
 
-def get_devs(filename):
-    capture = pyshark.FileCapture(filename)
+from aya.classes import WiFiDevice
 
-    print(capture[5])
+fc_types = {
+    8: "DATA_TYPE",
+    36: "TRIGGER_TYPE",
+    64: "PROBE_REQ_TYPE",
+    72: "NULL_TYPE",
+    80: "PROBE_RESP_TYPE",
+    84: "VHT_NDP_TYPE",
+    128: "BEACON_TYPE",
+    136: "QOS_DATA_TYPE",
+    148: "BLOCK_ACK_TYPE",
+    180: "RTS_TYPE",
+    200: "QOS_NULL_TYPE",
+    208: "ACTION_TYPE",
+    224: "NO_ACK_TYPE",
+}
+ap_types = ["BEACON_TYPE", "PROBE_RESP_TYPE"]
 
-get_devs('/home/sigsec/courtyardusm.pcap')
+
+def wifi_device_from_packet(packet: Packet) -> WiFiDevice:
+    """Create an Aya WiFiDevice from a pyshark Packets."""
+    dev_type = None
+    if packet.wlan.fc_type_subtype.hex_value in fc_types:
+        fc_type = fc_types[packet.wlan.fc_type_subtype.hex_value]
+        if fc_type in ap_types:
+            dev_type = "Wi-Fi AP"
+    return WiFiDevice(packet.wlan.ta, [], dev_type)
+
+
+def get_devs(filename: Path) -> list[WiFiDevice]:
+    """Generate a list of Aya WiFIDevices from a pcap."""
+    devs: dict[str, WiFiDevice] = {}
+    capture: pyshark.FileCapture = pyshark.FileCapture(filename)
+    for packet in capture:
+        if hasattr(packet.wlan, "ta"):
+            mac = packet.wlan.ta
+            if mac not in devs:
+                devs[mac] = wifi_device_from_packet(packet)
+    return list(devs.values())
+
+
+devices = get_devs(Path("/home/sigsec/courtyardusm.pcap"))
+for i in devices:
+    print(i)
